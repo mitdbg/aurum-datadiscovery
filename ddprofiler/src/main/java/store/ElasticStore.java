e0296e852f925a5b25508beaa49095af1a8f0d64
raulcf <raulcf@csail.mit.edu>
2016-05-27 13:14:49 -0400

    input documents into indices

--- ddprofiler/src/main/java/store/ElasticStore.java
+++ ddprofiler/src/main/java/store/ElasticStore.java
@@ -1,13 +1,16 @@
 package store;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import core.WorkerTaskResult;
 import core.config.ProfilerConfig;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Index;
 import io.searchbox.indices.CreateIndex;
 import io.searchbox.indices.mapping.PutMapping;
 
@@ -76,19 +79,46 @@ public class ElasticStore implements Store {
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
-		}	
+		}
 	}
 
 	@Override
 	public boolean indexData(int id, List<String> values) {
-		// TODO Auto-generated method stub
-		return false;
+		Map<String, String> source = new LinkedHashMap<String,String>();
+		String v = concatValues(values);
+		source.put("id", Integer.toString(id));
+		source.put("text", v);
+		Index index = new Index.Builder(source).index("text").type("column").build();
+		try {
+			client.execute(index);
+		} 
+		catch (IOException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean storeDocument(WorkerTaskResult wtr) {
-		// TODO Auto-generated method stub
-		return false;
+		Map<String, String> source = new LinkedHashMap<String,String>();
+		source.put("id", Integer.toString(wtr.getId()));
+		source.put("sourceName", wtr.getSourceName());
+		source.put("columnName", wtr.getColumnName());
+		source.put("dataType", wtr.getDataType());
+		source.put("totalValues", Integer.toString(wtr.getTotalValues()));
+		source.put("uniqueValues", Integer.toString(wtr.getUniqueValues()));
+		source.put("entities", wtr.getEntities().toString());
+		source.put("minValue", Float.toString(wtr.getMinValue()));
+		source.put("maxValue", Float.toString(wtr.getMaxValue()));
+		source.put("avgValue", Float.toString(wtr.getAvgValue()));
+		Index index = new Index.Builder(source).index("profile").type("column").build();
+		try {
+			client.execute(index);
+		} 
+		catch (IOException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
@@ -96,6 +126,16 @@ public class ElasticStore implements Store {
 		client.shutdownClient();
 		factory = null;
 		client = null;
+	}
+	
+	private String concatValues(List<String> values) {
+		StringBuilder sb = new StringBuilder();
+		String separator = " ";
+		for(String s : values) {
+			sb.append(s);
+			sb.append(separator);
+		}
+		return sb.toString();
 	}
 
 }
