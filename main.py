095ecec9fa7aba9654ab125c6a56a514b362186c
raulcf <raulcf@csail.mit.edu>
2016-07-11 16:57:30 -0400

    modified ipython iface with custom messages

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from IPython import embed
+from IPython.terminal.embed import InteractiveShellEmbed
 import sys
 
 from knowledgerepr import fieldnetwork
 from ddapi import API
 
+init_banner = "Welcome to Aurum. \nYou can access the API via the object api"
+exit_banner = "Bye!"
 
 def main(path_to_serialized_model):
     print('Loading: ' + str(path_to_serialized_model))
     network = fieldnetwork.deserialize_network(path_to_serialized_model)
     api = API(network)
     api.init_store()
-    embed()
+    ip_shell = InteractiveShellEmbed(banner1=init_banner, exit_msg=exit_banner)
+    ip_shell()
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
