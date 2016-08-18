f25aca6ecd1d79544f99ec1c6bc248846a23c602
raulcf <raulcf@csail.mit.edu>
2016-08-18 18:55:42 -0400

    skeleton for all tests

--- test_ddapi.py
+++ test_ddapi.py
@@ -16,6 +16,75 @@ class TestDDApiCombiner(unittest.TestCase):
 
     api = API(None)
 
+    """
+    Seed API
+    """
+
+    def test_drs_from_raw_field(self):
+        return
+
+    def test_drs_from_hit(self):
+        return
+
+    def test_drs_from_table(self):
+        return
+
+    def test_drs_from_table_hit(self):
+        return
+
+    """
+    Primitive API
+    """
+
+    def test_keyword_search(self):
+        return
+
+    def test_keywords_search(self):
+        return
+
+    def test_schema_name_search(self):
+        return
+
+    def test_schema_names_search(self):
+        return
+
+    def test_entity_search(self):
+        return
+
+    def test_schema_neighbors(self):
+        return
+
+    def test_schema_neighbors_of(self):
+        return
+
+    def test_similar_schema_name_to_field(self):
+        return
+
+    def test_similar_schema_name_to_table(self):
+        return
+
+    def test_similar_schema_name_to(self):
+        return
+
+    def test_similar_content_to_field(self):
+        return
+
+    def test_similar_content_to(self):
+        return
+
+    def test_pkfk_field(self):
+        return
+
+    def test_pkfk_table(self):
+        return
+
+    def test_pkfk_of(self):
+        return
+
+    """
+    Combiner API
+    """
+
     def test_intersection(self):
         a = DRS([1, 2, 3])
         b = DRS([3, 4, 5])
@@ -44,6 +113,19 @@ class TestDDApiCombiner(unittest.TestCase):
         self.assertTrue(3 not in res)
         self.assertTrue(4 not in res)
         self.assertTrue(5 not in res)
+
+
+    """
+    TC primitive API
+    """
+
+    def test_paths_between(self):
+        return
+
+    def test_paths(self):
+        return
+
+
 
 if __name__ == "__main__":
     unittest.main()
