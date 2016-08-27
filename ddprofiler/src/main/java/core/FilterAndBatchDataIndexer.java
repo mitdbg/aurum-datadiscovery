46c024624cfd074c775dacd85ba7d7661d26a1ff
raulcf <raulcf@csail.mit.edu>
2016-08-26 20:23:21 -0400

    Started sketching filterAndBatchDataIndexer to reduce number of indexes

--- ddprofiler/src/main/java/core/FilterAndBatchDataIndexer.java
+++ ddprofiler/src/main/java/core/FilterAndBatchDataIndexer.java
@@ -1,5 +1,8 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -11,9 +14,16 @@ import store.Store;
 public class FilterAndBatchDataIndexer implements DataIndexer {
 
 	private Store store;
+	private Map<String, Integer> attributeIds;
+	private Map<String, List<String>> attributeValues;
+	private Map<String, Integer> attributeValueSize;
+	private int indexTriggerThreshold = 1 * 1024 * 1024 * 35; // 35 MB 
 	
 	public FilterAndBatchDataIndexer(Store s) {
 		this.store = s;
+		this.attributeIds = new HashMap<>();
+		this.attributeValues = new HashMap<>();
+		this.attributeValueSize = new HashMap<>();
 	}
 	
 	/**
@@ -40,11 +50,56 @@ public class FilterAndBatchDataIndexer implements DataIndexer {
 			
 			if(at.equals(AttributeType.STRING)) {
 				String columnName = a.getColumnName();
+//				// Id for the attribute - computed only once
+//				int id = 0;
+//				if( ! attributeIds.containsKey(columnName)) {
+//					id = Utils.computeAttrId(sourceName, columnName);
+//					attributeIds.put(columnName, id);
+//				}
+//				else {
+//					id = attributeIds.get(columnName);
+//				}
+				
+				// FIXME: introduce new call
 				int id = Utils.computeAttrId(sourceName, columnName);
 				store.indexData(id, sourceName, columnName, entry.getValue().getStrings());
 			}
 		}
 		return true;
+	}
+	
+	private void storeNewValuesAndMaybeTriggerIndex(int id, String sourceName, String columnName, List<String> newValues) {
+		if(! attributeValues.containsKey(columnName)) {
+			attributeValues.put(columnName, new ArrayList<>());
+			attributeValueSize.put(columnName, 0);
+		}
+		int size = computeAproxSizeOf(newValues);
+		int currentSize = attributeValueSize.get(columnName);
+		int newSize = currentSize + size;
+		attributeValueSize.put(columnName, newSize);
+		updateValues(columnName, newValues);
+		if(newSize > indexTriggerThreshold) {
+			// Index the batch of values
+			List<String> values = attributeValues.get(columnName);
+			store.indexData(id, sourceName, columnName, values);
+			// Clean up
+			attributeValues.put(columnName, new ArrayList<>());
+			attributeValueSize.put(columnName, 0);
+		}
+	}
+	
+	private void updateValues(String columnName, List<String> values) {
+		List<String> currentValues = attributeValues.get(columnName);
+		currentValues.addAll(values);
+		attributeValues.put(columnName, currentValues);
+	}
+	
+	private int computeAproxSizeOf(List<String> values) {
+		int size = 0;
+		for(String s : values) {
+			size += s.length();
+		}
+		return size;
 	}
 
 	@Override
