055ed9ac740cb404eae8e36a8d5a068ef33160c0
Albert Carter <albert.r.carter@gmail.com>
2016-10-05 17:15:52 -0400

    add tests for other keyword search cases

--- test_algebra.py
+++ test_algebra.py
@@ -47,11 +47,37 @@ class testAlgebra(unittest.TestCase):
             max_results=max_results)
         self.assertEqual(result, 'return_drs')
 
+    @patch('algebra.DRS', MagicMock(return_value='return_drs'))
     def test_keyword_search_field(self):
-        pass
+        kw = 'foo'
+        scope = Scope.FIELD
+        max_results = 11
+        search_keyword = self.m_store_client.search_keyword
 
+        result = self.api.search_keyword(
+            kw=kw, scope=scope, max_results=max_results)
+
+        self.m_network.assert_not_called()
+        search_keyword.assert_called_with(
+            keywords=kw, elasticfieldname=KWType.KW_SCHEMA,
+            max_results=max_results)
+        self.assertEqual(result, 'return_drs')
+
+    @patch('algebra.DRS', MagicMock(return_value='return_drs'))
     def test_keyword_search_content(self):
-        pass
+        kw = 'foo'
+        scope = Scope.CONTENT
+        max_results = 11
+        search_keyword = self.m_store_client.search_keyword
+
+        result = self.api.search_keyword(
+            kw=kw, scope=scope, max_results=max_results)
+
+        self.m_network.assert_not_called()
+        search_keyword.assert_called_with(
+            keywords=kw, elasticfieldname=KWType.KW_TEXT,
+            max_results=max_results)
+        self.assertEqual(result, 'return_drs')
 
 
 if __name__ == '__main__':
