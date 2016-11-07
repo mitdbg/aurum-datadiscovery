53924423d7933f831cf255f5b3ba81ca2506292c
Albert Carter <albert.r.carter@gmail.com>
2016-11-07 15:06:39 -0500

    add nice error messages to sugar

--- sugar.py
+++ sugar.py
@@ -26,11 +26,11 @@ content = KWType.KW_TEXT  # content of the columns
 # Short variables for Relation
 # These represent edge types in the graph
 # and are used for neighbor searches
-schema = Relation.SCHEMA  # similar schemas
+# schema = Relation.SCHEMA  # similar schemas
 schema_sim = Relation.SCHEMA_SIM  # Similar Schema Names
 # similar content values. i.e. matching substrings and numbers
 content_sim = Relation.CONTENT_SIM
-entity_sim = Relation.ENTITY_SIM  # similar column names
+# entity_sim = Relation.ENTITY_SIM  # similar column names
 pkfk = Relation.PKFK  # join candidates
 
 # Short variables for api
@@ -45,35 +45,69 @@ difference = api.difference
 
 
 def search(kws, contexts=[source, field, content]):
-    if isinstance(contexts, KWType):
-        contexts = [contexts]
-    drs = None
-    if isinstance(kws, str):
-        kws = [kws]
+    try:
+        if isinstance(contexts, KWType):
+            contexts = [contexts]
+        drs = None
+        if isinstance(kws, str):
+            kws = [kws]
+        if isinstance(contexts, str):
+            raise Exception
 
-    for kw in kws:
-        for kwtype in contexts:
-            new_drs = keyword_search(kw, kwtype, max_results=500)
-            drs = union(drs, new_drs)
-    return drs
+        for kw in kws:
+            for kwtype in contexts:
+                new_drs = keyword_search(kw, kwtype, max_results=500)
+                drs = union(drs, new_drs)
+        return drs
+    except Exception:
+        msg = (
+            '--- Error ---' +
+            '\nThis function searches datasets for one or more keywords.'
+            '\nusage:\n\tsearch( (string|[string, string])' +
+            ' [, KWType | (KWTYpe, KWType, KWTYpe)] )' +
+            '\ne.g.:\n\tsearch(\'school\')' +
+            '\n\tsearch(\'school\', field)' +
+            '\n\tsearch([\'school\', \'education\'])' +
+            '\n\tsearch(\'school\', [source, field, content])')
+        print(msg)
 
 
 def neighbors(i_drs, relations=Relation, exclude_origin=True):
-    o_drs = None
+    try:
+        o_drs = None
 
-    if relations is None:
-        relations = [pkfk, content_sim, schema_sim]
-    if isinstance(relations, Relation):
-        relations = [relations]
+        if relations is None:
+            relations = [pkfk, content_sim, schema_sim]
+        if isinstance(relations, Relation):
+            relations = [relations]
+        if isinstance(relations, KWType):
+            raise ValueError(
+                'relation must be schema_sim, content_ sim, pkfk' +
+                'or an array of those values')
 
-    for relation in relations:
-        new_drs = neighbor_search(i_drs, relation)
-        o_drs = union(o_drs, new_drs)
+        for relation in relations:
+            new_drs = neighbor_search(i_drs, relation)
+            o_drs = union(o_drs, new_drs)
 
-    if exclude_origin:
-        o_drs = difference(o_drs, i_drs)
+        if exclude_origin:
+            o_drs = difference(o_drs, i_drs)
 
-    return o_drs
+        return o_drs
+    except:
+        msg = (
+            '--- Error ---' +
+            '\nThis function searches for neighbors of domain result set ' +
+            'or one of its precursors.' +
+            '\nusage:\n\tneighbors( ' +
+            '(drs/table name/hit id | [drs/hit/table name/hit id, ' +
+            'drs/hit/table name/hit id])' +
+            '\n\t\t[, Relation | (Relation, Relation, Relation)]' +
+            '\n\t\t[,exclude_origin=True/False] )' +
+            '\ne.g.:\n\tneighbors(\'Boston Capital Phase_pfp2-xvaj.csv\')' +
+            '\n\tneighbors(1600820766, schema_sim)' +
+            '\n\tneighbors([my_domain_result_set, [schema_sim, content_sim])' +
+            '\n\tneighbors([1600820766, my_drs], exclude_origin=False)')
+        print(msg)
 
 
 def paths_between(a, b, primitive):
@@ -81,12 +115,32 @@ def paths_between(a, b, primitive):
 
 
 def provenance(i_drs):
-    return i_drs.get_provenance().prov_graph().edges()
+    try:
+        return i_drs.get_provenance().prov_graph().edges()
+    except:
+        msg = (
+            '--- Error ---' +
+            '\nThis function returns a graph showing how a domain result set' +
+            'was reached.'
+            '\nusage:\n\tprovenance(drs)' +
+            '\ne.g.:\n\tmy_drs = search(\'school\')' +
+            '\n\tprovenance(my_drs)')
+        print(msg)
 
 
 def make_drs(general_input):
-    o_drs = api._general_to_drs(general_input)
-    return o_drs
+    try:
+        o_drs = api._general_to_drs(general_input)
+        return o_drs
+    except:
+        msg = (
+            '--- Error ---' +
+            '\nThis function returns domain result set from the supplied ' +
+            'input' +
+            '\nusage:\n\tmake_drs( table name/hit id | [table name/hit id, ' +
+            'drs/hit/string/int] )' +
+            '\ne.g.:\n\tmake_drs(1600820766)')
+        print(msg)
 
 
 if __name__ == '__sugar__':
