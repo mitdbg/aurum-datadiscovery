237bef0369b54c080553d8a9fcca38f2d11d00ae
raulcf <raulcf@csail.mit.edu>
2016-11-16 11:37:28 -0500

    Major code cleaning

--- old_dist_infra_py/celeryconfig.py
+++
@@ -1,6 +0,0 @@
-# Broker URL
-BROKER_URL = 'mongodb://localhost:27017/taskqueue'
-CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
-
-# Concurrency settings
-CELERYD_CONCURRENCY = 1
--- old_frontend/webconfig.py
+++
@@ -1,3 +0,0 @@
-host = '0.0.0.0'
-appdebug = True
-appname = "Aurum"
