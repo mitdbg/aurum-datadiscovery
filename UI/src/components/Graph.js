757ead3c11522cae516ca6ee193fe1c0e5e700f3
Albert Carter <albert.r.carter@gmail.com>
2016-12-13 14:24:04 -0500

    add basic sigma graph

--- UI/src/components/Graph.js
+++ UI/src/components/Graph.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigmajs';
 
 class Graph extends React.Component {
   render() {
 
    return (
-    <div id="graph">graph</div>
+    <div id="graph">
+      <Sigma graph={{nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]}} settings={{drawEdges:true}}>
+         <RelativeSize initialSize={15}/>
+         <RandomizeNodePositions/>
+      </Sigma>
+    </div>
     )
   }
 }
