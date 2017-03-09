67d3c76e2605d828dfdb2e5f70ee9a12aa635e23
Albert Carter <albert.r.carter@gmail.com>
2017-03-09 16:03:03 -0500

    remove crufty methods from graph.js. Move other components forward in react render hierarchy

--- UI/src/components/Graph.js
+++ UI/src/components/Graph.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import SourceMenu from './SourceMenu';
+import EdgeMenu from './EdgeMenu'
 import EmptyDiv from './EmptyDiv';
 import { makeRequest} from '../ajax'
-import { renderCanvas, removeOverlay } from '../labelCanvas'
 import {Sigma, EdgeShapes, ForceAtlas2, RandomizeNodePositions, RelativeSize} from 'react-sigma';
 import SigmaNode from './SigmaNode';
 import SigmaEdge from './SigmaEdge';
@@ -11,13 +11,6 @@ class Graph extends React.Component {
   constructor() {
     super();
     this.displayNodeDetails = this.displayNodeDetails.bind(this);
-    this.handleRequestResponse = this.handleRequestResponse.bind(this);
-    this.handleSourceResponse = this.handleSourceResponse.bind(this);
-    this.clearAndDrawNewLabels = this.clearAndDrawNewLabels.bind(this);
-    this.updateQueryEdgeType = this.updateQueryEdgeType.bind(this);
-    window.makeRequest = makeRequest;
-    window.handleRequestResponse = this.handleRequestResponse;
-    window.updateQueryEdgeType = this.updateQueryEdgeType; // call props.setQueryEdgeType
 
     this.state = {
       source: false,
@@ -53,38 +46,6 @@ class Graph extends React.Component {
 
         }
     };
-  }
-
-  updateQueryEdgeType(edgeType) {
-    // here b/c I haven't been able to work out how to pass this.props.setQueryEdgeType directly to the window.
-    this.props.setQueryEdgeType(edgeType);
-  }
-
-  clearAndDrawNewLabels(){
-    const selectedColumns = this.props.selection[this.state.source];
-    renderCanvas(
-      this.state.source, selectedColumns, this.state.columns, this.state.clickX, this.state.clickY);
-  }
-
-  handleRequestResponse(response) {
-    // little cheat to get the query from the URI
-    var url = decodeURI(response.responseURL);
-    var query = url.substr(url.lastIndexOf('/')+1);
-
-    const json = JSON.parse(response.responseText);
-
-    this.props.updateQuery(query, this.state.source);
-    this.props.updateResult(json);
-    removeOverlay();
-  }
-
-  // set state for columns after server api response
-  handleSourceResponse(response) {
-    const json = JSON.parse(response.responseText);
-    const columns = json.sources[this.state.source].field_res;
-
-    this.setState( { columns });
-    this.clearAndDrawNewLabels();
   }
 
   // read the graph object. call the api with the id, and then use another
@@ -124,6 +85,18 @@ class Graph extends React.Component {
             x={this.state.clickX}
             y={this.state.clickY}
           />
+          :
+          <EmptyDiv />
+        }
+
+        {this.state.source?
+          <div id="triangle"></div>
+          :
+          <EmptyDiv />
+        }
+
+        {this.state.source ?
+          <EdgeMenu />
           :
           <EmptyDiv />
         }
