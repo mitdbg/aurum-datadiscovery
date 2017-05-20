d3ee2f4e7be2ab4a177b64b08d0d923452db10aa
raulcf <raulcf@csail.mit.edu>
2017-05-20 18:22:29 -0400

    sketch of lite graph. small test to measure memory footprint

---
+++ knowledgerepr/test_lite_graph.py
@@ -0,0 +1,33 @@
+from knowledgerepr.lite_graph import LiteGraph
+from knowledgerepr.lite_graph import EdgeType
+import time
+
+if __name__ == "__main__":
+    g = LiteGraph()
+
+    num_nodes = 2000
+
+    for i in range(num_nodes):
+        g.add_node(i)
+
+    # complete graph
+
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if i != j:
+                g.add_undirected_edge(i, j, EdgeType.SCHEMA_SIM)
+
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if i != j:
+                g.add_undirected_edge(i, j, EdgeType.CONTENT_SIM)
+
+    for i in range(num_nodes):
+        for j in range(num_nodes):
+            if i != j:
+                g.add_undirected_edge(i, j, EdgeType.PKFK)
+
+    print("Nodes: " + str(g._node_count))
+    print("Edges: " + str(g._edge_count))
+
+    time.sleep(10)
