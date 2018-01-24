82b816bf3eb707ee5e7ea8bc710c8d90124e8cad
raulcf <raulcf@csail.mit.edu>
2018-01-23 19:44:51 -0500

    Included small util for key selection

--- DoD/data_processing_utils.py
+++ DoD/data_processing_utils.py
@@ -6,16 +6,13 @@ def join_ab_on_key(a, b, a_key, b_key):
     return joined
 
 
-def find_key_for(relation, attribute, value):
+def find_key_for(relation_path, key, attribute, value):
     """
-
-    :param relation:
-    :param attribute:
-    :param value:
-    :return:
+    select key from relation where attribute = value;
     """
-
-    return
+    df = pd.read_csv(relation_path, encoding='latin1')
+    key_value_df = df[df[attribute] == value][[key]]
+    return key_value_df.values[0][0]
 
 
 if __name__ == "__main__":
