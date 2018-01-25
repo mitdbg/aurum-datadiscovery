2c873c914956ca40ebfd53ac2f91ac5f635bd7b4
raulcf <raulcf@csail.mit.edu>
2018-01-24 20:54:54 -0500

    new hive connector; fix sqlserver connector

--- ddprofiler/src/main/java/core/tasks/ProfileTaskFactory.java
+++ ddprofiler/src/main/java/core/tasks/ProfileTaskFactory.java
@@ -1,29 +1,29 @@
 package core.tasks;
 
 import core.config.sources.CSVSourceConfig;
+import core.config.sources.HiveSourceConfig;
 import core.config.sources.PostgresSourceConfig;
 import core.config.sources.SQLServerSourceConfig;
 
 public class ProfileTaskFactory {
 
     public static ProfileTask makeCSVProfileTask(CSVSourceConfig config) {
-
 	ProfileTask pt = new CSVProfileTask(config);
-
 	return pt;
     }
 
     public static ProfileTask makePostgresProfileTask(PostgresSourceConfig config) {
-
 	ProfileTask pt = new PostgresProfileTask(config);
-
 	return pt;
     }
 
     public static ProfileTask makeSQLServerProfileTask(SQLServerSourceConfig config) {
-
 	ProfileTask pt = new SQLServerProfileTask(config);
+	return pt;
+    }
 
+    public static ProfileTask makeHiveProfileTask(HiveSourceConfig config) {
+	ProfileTask pt = new HiveProfileTask(config);
 	return pt;
     }
 
