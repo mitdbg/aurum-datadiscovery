009aa5a3b06f518c1aa294200ee7e129385326c1
raulcf@csail.mit.edu <raulcf@csail.mit.edu>
2018-05-24 20:25:31 -0400

    fix problem of candidate group with 1 table, this is now returned as the view; cleaning code

--- DoD/dod.py
+++ DoD/dod.py
@@ -142,8 +142,14 @@ class DoD:
             # continue
 
             if len(candidate_group) == 1:
-                print(str(candidate_group))
-                continue  # avoid the joinable for a 1-table group
+                # print(str(candidate_group))
+                table = candidate_group[0]
+                path = table_path[table]
+                # print(str(path))
+                materialized_virtual_schema = dpu.get_dataframe(path + "/" + table)
+                attrs_to_project = dpu.obtain_attributes_to_project((candidate_group_filters_covered, None))
+                yield materialized_virtual_schema, attrs_to_project
+                # continue  # avoid the joinable for a 1-table group
 
             # Pre-check
             # TODO: with a connected components index we can pre-filter many of those groups without checking
@@ -281,65 +287,6 @@ class DoD:
                                                     key=lambda x: x[1], reverse=True))
         for k, v in cache_unjoinable_pairs.items():
             print(str(k) + " => " + str(v))
-
-    def virtual_schema_exhaustive_search(self, list_attributes: [str], list_samples: [str]):
-
-        # Align schema definition and samples
-        assert len(list_attributes) == len(list_samples)
-        sch_def = {attr: value for attr, value in zip(list_attributes, list_samples)}
-
-        # Obtain sets that fulfill individual filters
-        filter_drs = dict()
-        for attr in sch_def.keys():
-            drs = self.api.search_attribute(attr)
-            filter_drs[(attr, FilterType.ATTR)] = drs
-
-        for cell in sch_def.values():
-            drs = self.api.search_content(cell)
-            filter_drs[(cell, FilterType.CELL)] = drs
-
-        # We group now into groups that convey multiple filters.
-        # Obtain list of tables ordered from more to fewer filters.
-        table_fulfilled_filters = defaultdict(list)
-        for filter, drs in filter_drs.items():
-            drs.set_table_mode()
-            for table in drs:
-                table_fulfilled_filters[table].append(filter)
-        # sort by value len -> # fulfilling filters
-        a = sorted(table_fulfilled_filters.items(), key=lambda el: len(el[1]), reverse=True)
-        table_fulfilled_filters = OrderedDict(sorted(table_fulfilled_filters.items(), key=lambda el: len(el[1]), reverse=True))
-
-        # Find all combinations of tables...
-        # Set cover problem, but enumerating all candidates, not just the minimum size set that covers the universe
-        candidate_groups = set()
-        num_tables = len(table_fulfilled_filters)
-        while num_tables > 0:
-            combinations = itertools.combinations(list(table_fulfilled_filters.keys()), num_tables)
-            for combination in combinations:
-                candidate_groups.add(frozenset(combination))
-            num_tables = num_tables - 1
-
-        # ...and order by coverage of filters
-        candidate_group_filters = defaultdict(list)
-        for candidate_group in candidate_groups:
-            filter_set = set()
-            for table in candidate_group:
-                for filter_covered_by_table in table_fulfilled_filters[table]:
-                    filter_set.add(filter_covered_by_table)
-            candidate_group_filters[candidate_group] = list(filter_set)
-        candidate_group_filters = OrderedDict(sorted(candidate_group_filters.items(), key=lambda el: len(el[1]), reverse=True))
-
-        # Now do all-pairs join paths for each group, and eliminate groups that do not join (future -> transform first)
-        joinable_groups = []
-        for candidate_group, filters in candidate_group_filters.items():
-            if len(candidate_group) > 1:
-                join_paths = self.joinable(candidate_group)
-                if join_paths > 0:
-                    joinable_groups.append((join_paths, filters))
-            else:
-                joinable_groups.append((candidate_group, filters))  # join not defined on a single table, so we add it
-
-        return joinable_groups
 
     def joinable(self, group_tables: [str], cache_unjoinable_pairs: defaultdict(int)):
         """
@@ -662,8 +609,10 @@ def obtain_table_paths(set_nids, dod):
 
 
 def test_e2e(dod, number_jps=5):
-    attrs = ["Mit Id", "Krb Name", "Hr Org Unit Title"]
-    values = ["968548423", "kimball", "Mechanical Engineering"]
+    # attrs = ["Mit Id", "Krb Name", "Hr Org Unit Title"]
+    # values = ["968548423", "kimball", "Mechanical Engineering"]
+    attrs = ["name", "c_phone"]
+    values = ["Customer#000000001", "25-989-741-2988"]
 
     # attrs = ["Last Name", "Building Name", "Bldg Gross Square Footage", "Department Name"]
     # values = ["Madden", "Ray and Maria Stata Center", "", "Dept of Electrical Engineering & Computer Science"]
@@ -728,11 +677,12 @@ if __name__ == "__main__":
     from knowledgerepr import fieldnetwork
     from modelstore.elasticstore import StoreHandler
     # basic test
-    path_to_serialized_model = "/Users/ra-mit/development/discovery_proto/models/newmitdwh/"
+    path_to_serialized_model = "/Users/ra-mit/development/discovery_proto/models/tpch/"
+    sep = "|"
     store_client = StoreHandler()
     network = fieldnetwork.deserialize_network(path_to_serialized_model)
 
-    dod = DoD(network=network, store_client=store_client)
+    dod = DoD(network=network, store_client=store_client, csv_separator=sep)
 
     test_e2e(dod, number_jps=10)
 
