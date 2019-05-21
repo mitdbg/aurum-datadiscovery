a1efd470671428ce8668f7db5938771f4e1bbee1
raulcf <raulcf@csail.mit.edu>
2019-05-21 16:13:14 -0400

    fixed early skip when checking for contradictory views

--- DoD/view_4c_analysis_baseline.py
+++ DoD/view_4c_analysis_baseline.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import pandas as pd
 from pandas.util import hash_pandas_object
 from DoD import material_view_analysis as mva
+import time
 
 from tqdm import tqdm
 
@@ -75,17 +76,9 @@ def are_they_complementary(t1, t2, md1, md2):
     return True, sdiff
 
 
-def brute_force_4c(dataframes_with_metadata):
-
-    # sort relations by cardinality to avoid reverse containment
-    dataframes_with_metadata = sorted(dataframes_with_metadata, key=lambda x: len(x[0]), reverse=True)
-
-    summarized_group = list()
-    candidate_complementary_group = list()
-    complementary_group = list()
-    contradictory_group = list()
-
+def summarize_views_and_find_candidate_complementary(dataframes_with_metadata):
     t_to_remove = set()
+    candidate_complementary_group = list()
     for t1, path1, md1 in dataframes_with_metadata:
         hashes1 = hash_pandas_object(t1)
         ht1 = hashes1.sum()
@@ -94,18 +87,18 @@ def brute_force_4c(dataframes_with_metadata):
         for t2, path2, md2 in dataframes_with_metadata:
             if path1 == path2:  # same table
                 continue
-            hashes2 = hash_pandas_object(t2)
-            ht2 = hashes2.sum()
             # if t2 is in remove group
             if path2 in t_to_remove:
                 continue
+            hashes2 = hash_pandas_object(t2)
+            ht2 = hashes2.sum()
 
             # are views are compatible
             if ht1 == ht2:
                 t_to_remove.add(path2)
             # are views potentially contained
             elif len(hashes1) > len(hashes2):
-                # are views contained
+                # is t2 contained in t1?
                 if len(set(hashes2) - set(hashes1)) == 0:
                     t_to_remove.add(path2)
             else:
@@ -125,34 +118,61 @@ def brute_force_4c(dataframes_with_metadata):
                     idx1 = [idx for idx, value in enumerate(hashes1) if value in s1_complement]
                     idx2 = [idx for idx, value in enumerate(hashes2) if value in s2_complement]
                     candidate_complementary_group.append((t1, md1, path1, idx1, t2, md2, path2, idx2))
+    return t_to_remove, candidate_complementary_group
+
+
+def pick_most_likely_key_of_pair(md1, md2):
+    most_likely_key1 = sorted(md1.items(), key=lambda x: x[1], reverse=True)
+    most_likely_key2 = sorted(md2.items(), key=lambda x: x[1], reverse=True)
+    candidate_k1 = most_likely_key1[0]
+    candidate_k2 = most_likely_key2[0]
+    # pick only one key so we make sure the group-by is compatible
+    if candidate_k1 >= candidate_k2:
+        k = candidate_k1[0]
+    else:
+        k = candidate_k2[0]
+    return k
+
+
+def brute_force_4c(dataframes_with_metadata):
+
+    # sort relations by cardinality to avoid reverse containment
+    dataframes_with_metadata = sorted(dataframes_with_metadata, key=lambda x: len(x[0]), reverse=True)
+
+    summarized_group = list()
+    complementary_group = list()
+    contradictory_group = list()
+
+    t_to_remove, candidate_complementary_group = summarize_views_and_find_candidate_complementary(dataframes_with_metadata)
 
     contradictory_pairs = set()
-    complementary_pairs = set()
+
+    times = []
 
     # now we'd check contradictory
+    print("Pairs of candidate complementary: " + str(len(candidate_complementary_group)))
     for t1, md1, path1, idx1, t2, md2, path2, idx2 in tqdm(candidate_complementary_group):
+        print(path1 + " -> " + path2)
+    for t1, md1, path1, idx1, t2, md2, path2, idx2 in tqdm(candidate_complementary_group):
+        s = time.time()
         if path1 in t_to_remove or path2 in t_to_remove:
             continue  # this will be removed, no need to worry about them
-        selection1 = t1.iloc[idx1]
-        selection2 = t2.iloc[idx2]
-        mlk1 = sorted(md1.items(), key=lambda x: x[1], reverse=True)
-        mlk2 = sorted(md2.items(), key=lambda x: x[1], reverse=True)
-        candidate_k1 = mlk1[0]
-        candidate_k2 = mlk2[0]
-        # pick only one key so we make sure the group-by is compatible
-        if candidate_k1 >= candidate_k2:
-            k = candidate_k1[0]
-        else:
-            k = candidate_k2[0]
+
+        k = pick_most_likely_key_of_pair(md1, md2)
 
         complementary_key1 = set()
         complementary_key2 = set()
         contradictory_key1 = set()
         contradictory_key2 = set()
 
+        selection1 = t1.iloc[idx1]
+        selection2 = t2.iloc[idx2]
+
         s1 = selection1[k]
         s2 = set(selection2[k])
         for key in s1:
+            if len(contradictory_key1) > 0:  # check this condition for early skip
+                break
             for c in selection1.columns:
                 cell_value1 = set(selection1[selection1[k] == key][c])
                 if key in s2:
@@ -166,6 +186,8 @@ def brute_force_4c(dataframes_with_metadata):
             s2 = set(selection2[k]) - set(s1)  # we only check the set difference to save some lookups
             s1 = set(selection1[k])
             for key in s2:
+                if len(contradictory_key2) > 0:  # check this condition for early skip
+                    break
                 for c in selection2.columns:
                     cell_value2 = set(selection2[selection2[k] == key][c])
                     if key in s1:
@@ -179,28 +201,24 @@ def brute_force_4c(dataframes_with_metadata):
             contradictory_group.append((path1, k, contradictory_key1, path2))
             contradictory_pairs.add(path1 + "%$%" + path2)
             contradictory_pairs.add(path2 + "%$%" + path1)
-            # for ck1 in contradictory_key1:
-            #     contradictory_group.append((path1, k, ck1, path2))
         if len(contradictory_key2) > 0:
             contradictory_group.append((path2, k, contradictory_key2, path1))
             contradictory_pairs.add(path1 + "%$%" + path2)
             contradictory_pairs.add(path2 + "%$%" + path1)
-            # for ck2 in contradictory_key2:
-            #     contradictory_group.append((path2, k, ck2, path1))
         if len(contradictory_key1) == 0 and len(contradictory_key2) == 0:
             if path1 + "%$%" + path2 in contradictory_group or path2 + "%$%" + path1 in contradictory_group:
                 continue
             else:
                 complementary_group.append((path1, path2, complementary_key1, complementary_key2))
-            # for cmk1 in complementary_key1:
-            #     complementary_group.append((path1, k, cmk1))
-            # for cmk2 in complementary_key2:
-            #     complementary_group.append((path2, k, cmk2))
 
+        e = time.time()
+        t = e - s
+        times.append(t)
     # summarize out contained and compatible views
     for t, path, md in dataframes_with_metadata:
         if path not in t_to_remove:
             summarized_group.append(path)
+    print("TIMES: " + str(times))
     return summarized_group, complementary_group, contradictory_group
 
 
@@ -211,6 +229,7 @@ def brute_force_4c_valuewise(dataframes_with_metadata):
     contradictory_group = list()
 
     t_to_remove = set()
+
     for t1, path1, md1 in dataframes_with_metadata:
         # let's first detect whether this view is going to be removed
         ht1 = hash_pandas_object(t1).sum()
@@ -303,34 +322,3 @@ if __name__ == "__main__":
         print("Contradictory views: ")
         for path1, _, _, path2 in contradictory_group:
             print(path1 + " - " + path2)
-
-
-
-    # group by how well they fulfill definition - and maybe cleanliness
-
-
-
-
-## old way of checking complementary -> not debugged, robust to syntactic caps
-
-# if len(t1) == len(t2):
-#     are_compatible = True
-#     for c in t1.columns:
-#         s1 = t1[c].apply(lambda x: str(x).lower()).sort_values().reset_index(drop=True)
-#         s2 = t2[c].apply(lambda x: str(x).lower()).sort_values().reset_index(drop=True)
-#         idx = (s1 == s2)
-#         if not idx.all():
-#             are_compatible = False
-#             break
-#     if are_compatible:
-#         t_to_remove.add(hash_pandas_object(t2).sum())
-#     else:
-#         # They are not compatible, they may be contradictory
-#         contradictory, contradictions = are_views_contradictory(t1, t2, md1, md2)
-#         if contradictory:
-#             contradictory_group.add((path1, path2, contradictions))
-#         else:
-#             # not contradictory, are they complementary?
-#             complementary, complement = are_they_complementary(t1, t2, md1, md2)
-#             if complementary:
-#                 complementary_group.add((path1, path2, complement))
